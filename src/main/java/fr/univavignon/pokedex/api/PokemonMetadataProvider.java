e4dbd4013b4502b89c733f9d5cc3a7c7e30732b4
ndeye beye <ndeye.beye@etu.univ-amu.fr>
2024-11-13 00:08:18 +0100

    TP4 : - Implémentation des classes de chaque Interface avec leurs Tests

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -14,11 +14,6 @@ public class PokemonMetadataProvider implements IPokemonMetadataProvider{
 
     @Override
     public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-        if (index < 0 || index >= metadataList.size()) {
-            throw new PokedexException("Aucune métadonnée trouvée pour l'index : " + index);
-        }
-        Pokemon pokemonExample1 = new Pokemon(0, "Bulbizarre", 126,126,90,613,64,4000,4,56);
-        Pokemon pokemonExample2 = new Pokemon(133, "Aquali", 186,168,260,2729,202,5000,4,100);
 
         return this.metadataList.get(index);
     }
