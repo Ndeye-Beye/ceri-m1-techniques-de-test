58129875b31a69a5fa26d4f60cbd8091140f23ee
ndeye beye <ndeye.beye@etu.univ-amu.fr>
2024-11-13 11:24:46 +0100

    TP4 : - Implémentation des classes de chaque Interface avec leurs Tests

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -1,4 +1,22 @@
-package fr.univavignon.pokedex.api;
+/*package fr.univavignon.pokedex.api;
 
-public class PokemonTrainerFactory {
-}
+import java.util.List;
+import java.util.ListResourceBundle;
+
+public class PokemonTrainerFactory implements IPokemonTrainerFactory{
+    private List<PokemonTrainer> pokemonTrainerList;
+
+    public PokemonTrainerFactory(List<PokemonTrainer> pokemonTrainerList){
+        this.pokemonTrainerList = pokemonTrainerList;
+    }
+    @Override
+    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+
+        IPokemonMetadataProvider pokemonMetadataProvider = null;
+        PokemonMetadata metadata = pokemonMetadataProvider.getPokemonMetadata()
+        IPokemonFactory pokemonFactory = ;
+
+        PokemonTrainer pokemonTrainer = new PokemonTrainer(name, team, pokedexFactory.createPokedex());
+        return null;
+    }
+}*/
