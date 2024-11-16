2d948bec16a08226d9c3edac7cacfc68a08797ee
ndeye beye <ndeye.beye@etu.univ-amu.fr>
2024-11-16 19:09:31 +0100

    TP4 : ajout de l'implémentation de la classe PokemonTrainerFactory.java et PokemonTrainerFactoryTest.java

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -9,15 +9,6 @@ public class PokemonTrainerFactory implements IPokemonTrainerFactory{
     @Override
     public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
 
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("le nom n'est pas null");
-        }
-        if (team == null) {
-            throw new IllegalArgumentException("Team n'est pas null.");
-        }
-        if (pokedexFactory == null) {
-            throw new IllegalArgumentException("PokedexFactory n'est pas null.");
-        }
 
         List<PokemonMetadata> metadataList = new ArrayList<>();
         metadataList.add(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
